4e7cff526e6c9008efbd9faea3320a4fab661930
Sohil Chanana <schanana2003@gmail.com>
2024-12-02 22:10:12 -0500

    minor changes

--- src/app/submit-image/page.tsx
+++ src/app/submit-image/page.tsx
@@ -16,7 +16,7 @@ export default function SubmitImage() {
         </div>
         <p className="mt-3 text-xl text-gray-300 font-bold">Submit an Image</p>
       </div>
-      <div className="flex flex-col w-full items-center p-4 bg-[#424242] rounded-xl m-4">
+      <div className="flex flex-col w-full items-center p-4 bg-[#424242] rounded-xl my-4">
         <MapSIProvider>
           <div className="flex flex-col items-center mt-4 w-full">
             <SubmitImageButton />
